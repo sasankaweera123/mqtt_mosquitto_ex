97f79422c9692870c2d04eddde55a4b268a211f5
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-03-18 12:31:34 +0530

    - Update Files - Add new mosquitto_sub file

--- mqtt_mos_ex/main.cpp
+++ mqtt_mos_ex/main.cpp
@@ -6,20 +6,27 @@ int main() {
     int rc;
     struct mosquitto *mosq;
 
+    std::cout << "Initializing mosquitto library" << std::endl;
+
     mosquitto_lib_init();
 
     mosq = mosquitto_new("publisher-test", true, NULL);
 
     if (mosq) {
         rc = mosquitto_connect(mosq, "localhost", 1883, 60);
-        if (rc == MOSQ_ERR_SUCCESS) {
-            mosquitto_publish(mosq, NULL, "test", 4, "Hello", 0, false);
-            mosquitto_disconnect(mosq);
-        } else {
+        if (rc != 0) {
             std::cout << "Error: " << rc << std::endl;
+            mosquitto_destroy(mosq);
+            return -1;
         }
+        std::cout << "Connected" << std::endl;
+        mosquitto_publish(mosq, NULL, "test/t1", 6, "test", 0, false);
+        std::cout << "Published" << std::endl;
+        mosquitto_disconnect(mosq);
         mosquitto_destroy(mosq);
     }
+
+    std::cout << "Cleaning up" << std::endl;
 
     mosquitto_lib_cleanup();
 
