37e637a8a8e7322df7b784f8732e9231ec9eb35c
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-03-18 10:28:44 +0530

    Add dynamic lib for the project

---
+++ mqtt_mos_ex/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+//#include "include/mosquitto.h" // This is the include from static library
+#include <mosquitto.h> // This is the correct include from dynamic library
+
+int main() {
+    int rc;
+    struct mosquitto *mosq;
+
+    mosquitto_lib_init();
+
+    mosq = mosquitto_new("publisher-test", true, NULL);
+
+    if (mosq) {
+        rc = mosquitto_connect(mosq, "localhost", 1883, 60);
+        if (rc == MOSQ_ERR_SUCCESS) {
+            mosquitto_publish(mosq, NULL, "test", 4, "Hello", 0, false);
+            mosquitto_disconnect(mosq);
+        } else {
+            std::cout << "Error: " << rc << std::endl;
+        }
+        mosquitto_destroy(mosq);
+    }
+
+    mosquitto_lib_cleanup();
+
+    return 0;
+}
